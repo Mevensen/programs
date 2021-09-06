e57deb6074cf1945099a4d3a65baf02017519ccb
Mevensen <71598617+Mevensen@users.noreply.github.com>
2021-09-06 12:48:20 +0200

    Create programs.sh
    Add programs.sh with files clone script.

---
+++ programs.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/safflower/phpmyadmin-authentication-bruteforce.git #phpmyadmin bruteforcer
+git clone https://github.com/bitsadmin/wesng.git #windows exploit suggestions from systeminfo.txt
+git clone https://github.com/WhiteWinterWolf/wwwolf-php-webshell.git #webshell with upload and execution
